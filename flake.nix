cc2603ced6ebc19e5838e3a1aac81a95323ea976
brudel <brudel13742@gmail.com>
2024-11-21 23:22:00 -0300

    Fix flake package name

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs { inherit system; };
-        package = "misterio-me";
+        package = "brudel-dev";
       in {
         packages.${package} = pkgs.stdenv.mkDerivation {
           pname = package;
